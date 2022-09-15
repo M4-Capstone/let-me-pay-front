77ce269f4c811613776de56e6313db0bb30fac91
antoniel2 <antoniel@q9.com.br>
2022-09-15 00:06:36 -0300

    Fix: Fixing routes with token valid

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,34 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import Dashboard from "../pages/dashboard.page";
 import Login from "../pages/login.page";
 import Register from "../pages/register.page";
+import { isJwtExpired } from 'jwt-check-expiration';
+import { useEffect, useState } from "react";
 
 const Routes = () => {
+
+  const history = useHistory()
+
+
+  useEffect(()=>{
+    const token = localStorage.getItem("@tokenLMP")
+    
+    if(token){
+
+      const verified = isJwtExpired(token)
+
+      if(!verified){
+        history.push("/dashboard")
+      }else{
+        history.push("/login")
+      }
+    }
+  }, [])
+
+  
   return (
     <Switch>
+      
       <Route exact path="/login">
         <Login />
       </Route>
@@ -18,7 +41,6 @@ const Routes = () => {
         <Dashboard />
       </Route>
 
-      <Redirect from="/" to="dashboard" />
     </Switch>
   );
 };
